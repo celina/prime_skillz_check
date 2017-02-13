82dbd703aa1528bcd1617aafb50b8125711f5485
Celina Peterson <celinap@gmail.com>
2017-02-13 14:20:15 -0600

    completed 6-functions

--- 6-functions/func.js
+++ 6-functions/func.js
@@ -2,12 +2,25 @@
 // that takes no arguments and
 // returns the value 4
 
+var q1 = function() {
+   return 4;
+};
+
 // 2 - Write a function declaration
 // that takes two arguments and
 // returns the value of the result
 // of the multiplication of both
 // arguments
 
+var q2 = function(a, b) {
+   return a * b;
+};
+
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
+
+var q3 = function(a) {
+  return a;
+  console.log(a);
+}
